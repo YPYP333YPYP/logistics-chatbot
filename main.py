f6d08d82269425219b5e3c6c79d1a79d0d29254e
YPYP333YPYP <ypyp333ypyp@gmail.com>
2025-03-17 12:29:41 +0900

    refactor: table 자동 생성을 위한 main.py 모델 import

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from model import carrier, shipping
+from model import carrier, logistics_shipment
 from db.database import init_db, close_db
 
 
