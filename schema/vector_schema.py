31f8aab2e2eb8cf7516dafb3c33298d7a6cf1ecb
YPYP333YPYP <ypyp333ypyp@gmail.com>
2025-03-18 18:32:09 +0900

    feat: QueryModel 스키마 생성

--- schema/vector_schema.py
+++ schema/vector_schema.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Dict, Any
 
 from pydantic import BaseModel
 
@@ -6,3 +6,9 @@ from pydantic import BaseModel
 class RebuildModel(BaseModel):
     force_rebuild: Optional[bool] = False
     limit: Optional[int] = None
+
+
+class QueryModel(BaseModel):
+    query: str
+    k: Optional[int] = 5
+    filter_dict: Optional[Dict[str, Any]] = None
